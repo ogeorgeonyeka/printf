0a568178d4a8c77ed92775eb532bf0aa0f370533
george okoronkwo <ogeorgeonyeka@gmail.com>
2023-04-26 01:39:59 -0700

    printf projects

--- main.c
+++ main.c
@@ -38,7 +38,7 @@ int main(void)
 	len2 = printf("Percent:[%%]\n");
 	_printf("Len:[%d]\n", len);
 	printf("Len:[%d]\n", len2);
-	_printf("Unknown:[%s]\n");
-	printf("Unknown: [%s]\n"); 
+	_printf("Unknown:[%p]\n", addr);
+	printf("Unknown: [%p]\n", addr); 
 	return (0);
 }
